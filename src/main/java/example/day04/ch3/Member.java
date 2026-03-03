4f9a6a7370e4290c020b698128c75f9cdde5ab4a
bbrrrvsg <myeonghui1813@gmail.com>
2026-03-03 15:43:45 +0900

    2026-2-27

---
+++ src/main/java/example/day04/ch3/Member.java
@@ -0,0 +1,20 @@
+package example.day04.ch3;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 빈생성자 이면서 protected 접근제한
+@AllArgsConstructor // 전체 매개변수를 갖는 생성자
+@Getter // getter 메소드 자동생성
+@Setter
+@Entity // 데이터베이스의 테이블 레코드와 매핑(연결) 기술 : ORM(자바 객체 <--> DB레코드)
+public class Member {
+
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    @Column(name = "id" , updatable = false) // 필드/속성 설정 , 수정 불가능
+    private Long id;
+
+    @Column(name = "name", nullable = false) // 필드/속성명 설정 , not null
+    private String name;
+}
---
+++ src/main/java/example/day04/ch3/MemberRepository.java
@@ -0,0 +1,13 @@
+package example.day04.ch3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // 리포지토리 계층(퍼시스턴스 계층)
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    // 1. 인터페이스 위에 @Repository
+    // 2. JpaRepository로부터 상속 받는다
+    // 3. JpaRepository<매핑클래스명,pk타입>
+    // * <> 제네릭 타입
+}
